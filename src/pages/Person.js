3346248af1de4580276490c53f22cddc67be6e3c
Miles Weber <vorvon4@gmail.com>
2021-12-01 12:03:14 -0500

    Setup Styling for About Pages

--- src/pages/Person.js
+++ src/pages/Person.js
@@ -1,14 +1,17 @@
 import React from "react";
 import '../App.css';
+import './styles/Person.css';
 
 function Person(props){
     return(
-        <div className="App App-bkg">
+        <div className="App-bkg">
         <div className="container mb-3 App-bkg text-light">
             <br />
             <h1 className="">About {props.name}</h1>
-            <div>
-                <img src={props.image} alt={props.name} className="img-fluid" />
+            <div className="border container about-container p-3">
+                <img src={props.image} alt={props.name} className="w-25 me-3 mb-3 thumbnail img-thumbnail" />                
+                <p className = "thumbnail-text">Some placeholder text about {props.name}</p>
+                
             </div>
         </div>
         </div>
