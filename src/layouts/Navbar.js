783fdb81b64bb062fca86c8e9c1955e8e26b0ff7
Miles Weber <vorvon4@gmail.com>
2022-01-10 13:25:12 -0500

    Added page not finished to unfinished pages with links that work

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -5,17 +5,20 @@ function Navbar(){
     return(
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-            <div class="navbar-brand">
-                <img src={logo} width="30" height="30" className="d-inline-block align-top" alt=""/>
-                What's Poppin?
-            </div>
-        </Link>
+            <Link className="navbar-brand" to="/">
+                <div class="navbar-brand">
+                    <img src={logo} width="30" height="30" className="d-inline-block align-top" alt=""/>
+                    What's Poppin?
+                </div>
+            </Link>
+        
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">            
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item"><Link className = "nav-link" to="/">Home</Link></li>
                 <li className="nav-item dropdown">
                     {/* eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
                     <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
--- src/pages/Person.js
+++ src/pages/Person.js
@@ -7,20 +7,24 @@ function Person(props){
     var about = props.json.about;
     var link = props.json.external_link;
     var link_text = props.json.external_link_text;
-    return(
-        <div className="App-bkg">
-        <div className="container mb-3 App-bkg text-light">
-            <br />
-            <h1 className="">About {full_name}</h1>
-            <div className="border container about-container p-3">
-                <img src={props.image} alt={'Picture of ' + props.name} className="w-25 me-3 mb-3 thumbnail img-thumbnail" />                
-                <p className = "thumbnail-text">{about}</p>
-                {/* eslint-disable-next-line react/jsx-no-target-blank*/}
-                <a href={link} target = "_blank" rel = "noreferer" className="btn btn-primary">Link to {link_text}</a>
-                
+    if(about === null || about === '')
+        return(<div className="container text-center mt-5 mb-5"><h1 className="text-danger"><i className="bi bi-exclamation-triangle"></i> Page not finished</h1></div>)
+    else
+        return(
+            
+            <div className="App-bkg">
+            <div className="container mb-3 App-bkg text-light">
+                <br />
+                <h1 className="">About {full_name}</h1>
+                <div className="border container about-container p-3">
+                    <img src={props.image} alt={'Picture of ' + props.name} className="w-25 me-3 mb-3 thumbnail img-thumbnail" />                
+                    <p className = "thumbnail-text">{about}</p>
+                    {/* eslint-disable-next-line react/jsx-no-target-blank*/}
+                    <a href={link} target = "_blank" rel = "noreferer" className="btn btn-primary">Link to {link_text}</a>
+                    
+                </div>
             </div>
-        </div>
-        </div>
-    )
+            </div>
+        )
 }
 export default Person;
